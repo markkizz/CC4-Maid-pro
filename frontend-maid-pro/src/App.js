d097c7784a42838a385c2cd79417066a1d22421c
markkizz <mark.pattanapara@gmail.com>
2020-01-14 01:21:40 +0700

    Finish static MaidCard component

--- frontend-maid-pro/src/App.js
+++ frontend-maid-pro/src/App.js
@@ -1,11 +1,8 @@
 import React from "react";
 import "./App.css";
-// import { Button } from "antd";
 
 function App() {
-  return (
-    <div className="App">{/* <Button type="primary">click</Button> */}</div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
