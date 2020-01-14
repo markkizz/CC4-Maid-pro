783ed338e68cae9e253669c1e567beea1feca71d
markkizz <mark.pattanapara@gmail.com>
2020-01-14 11:22:40 +0700

    Finish static ReviewCard component

--- frontend-maid-pro/src/App.js
+++ frontend-maid-pro/src/App.js
@@ -1,8 +1,13 @@
 import React, { Component } from "react";
 import "./App.css";
+import ReviewCard from "./components/ReviewCard/ReviewCard";
 export class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <ReviewCard />
+      </div>
+    );
   }
 }
 
