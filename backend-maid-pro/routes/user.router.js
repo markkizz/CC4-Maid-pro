b517ab849e0c55f8388940447b9bf2aef61f28ee
fResult <styxmaz@gmail.com>
2020-01-14 12:40:51 +0700

    Fixed error Register path API

--- backend-maid-pro/routes/user.router.js
+++ backend-maid-pro/routes/user.router.js
@@ -3,6 +3,14 @@ const userController = require('../controllers/user.controller');
 module.exports = (server, db) => {
   const controller = userController(db);
 
-  server.post('users/register', controller.signUp);
+  server.post('/users/register', controller.signUp);
+
+  server.post('/users/sign-in', controller.signIn);
+
+  server.get('/users/:id', (req, res) => {
+    const result = req.query.type;
+    console.log(result, req.params.id)
+  });
+
 
 };
