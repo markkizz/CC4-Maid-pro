51d261d3240f5b96585e8620f4180ad6517b5cf9
mine01 <patteera.banl@gmail.com>
2020-01-16 14:13:29 +0700

    findBookings not complete

--- backend-maid-pro/routes/booking.router.js
+++ backend-maid-pro/routes/booking.router.js
@@ -1,5 +1,8 @@
-const bookingController = require('../controllers/booking.controller')
+const bookingController = require('../controllers/booking.contfroller')
 module.exports = (server, db) => {
     const controller = bookingController(db)
     server.post('/bookings', controller.createBooking);
+    server.get('/bookings/employers/:employerId', controller.findBookingsByEmployerId);
 };
+
+ 
