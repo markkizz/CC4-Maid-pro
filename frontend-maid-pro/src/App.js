76831cc33c728b789e9de89c40c6358385f3b535
markkizz <mark.pattanapara@gmail.com>
2020-01-17 12:21:23 +0700

    Fix conflict from develop to mark_searchpage

--- frontend-maid-pro/src/App.js
+++ frontend-maid-pro/src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
-import BookingCard from "./components/BookingCard/BookingCard";
 
 export class App extends Component {
   render() {
-    return <div className="App">
-      <BookingCard/>
-    </div>;
+    return <div className="App"></div>;
   }
 }
 
