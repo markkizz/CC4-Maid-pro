6e103d1bee6d9029cd97d7890c3536fe29c5292d
bellchutinun25bacon <bellchutinun25bacon@gmail.com>
2020-01-18 14:11:38 +0700

    finish cancel booking

--- frontend-maid-pro/src/App.js
+++ frontend-maid-pro/src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
-import BookingCard from '../src/components/BookingCard/BookingCard'
 export class App extends Component {
   render() {
     return <div className="App">
-      <BookingCard />
+
     </div>;
   }
 }
