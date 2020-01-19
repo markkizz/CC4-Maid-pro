377e584a499b625aa881d758fa6a2e6a9e34b9c9
Hon <dennis.mejen@gmail.com>
2020-01-20 00:37:49 +0700

    Merge hon_Registration

--- frontend-maid-pro/src/App.js
+++ frontend-maid-pro/src/App.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import "./App.css";
-import Registration from './pages/Registration/Registration'
 export class App extends Component {
   render() {
-    return <div className="App">
-      <Registration />
-    </div>;
+    return <div className="App"></div>;
   }
 }
 
