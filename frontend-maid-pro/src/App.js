40dca46a13ed8ce6b3dc5594c4f04522715ea58d
Hon <dennis.mejen@gmail.com>
2020-01-20 00:45:03 +0700

    Add RegistrationMaid

--- frontend-maid-pro/src/App.js
+++ frontend-maid-pro/src/App.js
@@ -1,8 +1,11 @@
 import React, { Component } from "react";
 import "./App.css";
+import RegistrationMaid from './pages/RegistrationMaid/RegistrationMaid'
 export class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return <div className="App">
+      <RegistrationMaid />
+    </div>;
   }
 }
 
