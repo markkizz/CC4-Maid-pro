201ddee4b12989ec049d47785e64c29ed60faefb
markkizz <mark.pattanapara@gmail.com>
2020-01-20 09:53:54 +0700

    Add getMaidBooking

--- backend-maid-pro/routes/booking.router.js
+++ backend-maid-pro/routes/booking.router.js
@@ -1,8 +1,10 @@
+const passport = require('passport')
 const bookingController = require('../controllers/booking.controller')
 module.exports = (server, db) => {
     const controller = bookingController(db)
     server.post('/bookings', controller.createBooking);
+    // ? should pass with auth
     server.get('/bookings/employers/:employerId', controller.findBookingsByEmployerId);
-};
 
- 
+    server.get('/bookings/maids/',passport.authenticate('jwt', { session: false }), controller.findBookingsByMaidId)
+};
