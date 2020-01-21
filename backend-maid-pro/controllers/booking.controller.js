beadfb9e1fddd26d4e179fd21234bc9f61479632
markkizz <mark.pattanapara@gmail.com>
2020-01-21 23:03:27 +0700

    Add find employer booking with maid data

--- backend-maid-pro/controllers/booking.controller.js
+++ backend-maid-pro/controllers/booking.controller.js
@@ -12,18 +12,17 @@ module.exports = db => {
             maid_id: maidId
           }
         });
-        if (userBooking && userBooking.dataValues.status !== 'ACCEPT')
+        if (userBooking && userBooking.dataValues.status !== "ACCEPT")
           res.status(400).json({ errorMessage: "User already booked" });
-        const result = await
-          db.booking.create({
-            customer_location: req.body.customer_location,
-            work_date: req.body.work_date,
-            work_hour: req.body.work_hour,
-            status: 'WAIT_FOR_ACCEPTANCE',
-            pay_slip_image: req.body.pay_slip_image,
-            employer_id: req.user.id,
-            maid_id: maidId
-          });
+        const result = await db.booking.create({
+          customer_location: req.body.customer_location,
+          work_date: req.body.work_date,
+          work_hour: req.body.work_hour,
+          status: "WAIT_FOR_ACCEPTANCE",
+          pay_slip_image: req.body.pay_slip_image,
+          employer_id: req.user.id,
+          maid_id: maidId
+        });
         if (result.length === 0) {
           res.status(204).json({ result });
         } else {
@@ -39,15 +38,34 @@ module.exports = db => {
 
     findBookingsByEmployerId: async (req, res) => {
       try {
+        const employer = req.user;
+        if (employer.type !== "EMPLOYER")
+          res.status(400).json({ errorMessage: "Unauthorized" });
         const result = await db.booking.findAll({
           where: {
-            employer_id: req.params.employerId
+            employer_id: req.user.id
           }
         });
-        if (result.length === 0) {
-          res.status(204).json(result);
+        const resultMaidId = result.map(maid => maid.dataValues.maid_id);
+        let maidData = [];
+        for (let i = 0; i < resultMaidId.length; i++) {
+          let maid = await db.user.findOne({
+            where: { id: resultMaidId[i] },
+            attributes: [
+              "username",
+              "first_name",
+              "last_name",
+              "profile_img"
+            ]
+          });
+          maidData.push(maid.dataValues);
+        }
+        const newResult = result.map((maid, i) => ({...maid.dataValues, maid_data: maidData[i] }))
+        console.log(newResult)
+        if (newResult.length === 0) {
+          res.status(204).json(newResult);
         } else {
-          res.status(200).json(result);
+          res.status(200).json(newResult);
         }
       } catch (err) {
         if (err.message.includes("ECONNREFUSED")) {
--- backend-maid-pro/routes/booking.router.js
+++ backend-maid-pro/routes/booking.router.js
@@ -3,8 +3,8 @@ const bookingController = require('../controllers/booking.controller')
 module.exports = (server, db) => {
     const controller = bookingController(db)
     server.post('/bookings/maids/:maidId', passport.authenticate('jwt', { session: false }), controller.createBooking);
-    // ? should pass with auth
-    server.get('/bookings/employers/:employerId', controller.findBookingsByEmployerId);
+
+    server.get('/bookings/employers/', passport.authenticate('jwt', { session: false }), controller.findBookingsByEmployerId);
 
     server.get('/bookings/maids/',passport.authenticate('jwt', { session: false }), controller.findBookingsByMaidId)
 };
