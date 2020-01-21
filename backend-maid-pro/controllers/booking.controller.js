93ea7401c76a71a17c0d859869d0b03de87ad8d9
markkizz <mark.pattanapara@gmail.com>
2020-01-21 20:23:58 +0700

    Edit employer createBooking api

--- backend-maid-pro/controllers/booking.controller.js
+++ backend-maid-pro/controllers/booking.controller.js
@@ -1,68 +1,78 @@
-const Sequelize = require('sequelize');
+const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
-module.exports = (db) => {
+module.exports = db => {
   return {
     createBooking: async (req, res) => {
       try {
+        const { maidId } = req.params;
+        const userBooking = await db.booking.findOne({
+          where: {
+            employer_id: req.user.id,
+            maid_id: maidId
+          }
+        });
+        if (userBooking && userBooking.dataValues.status !== 'ACCEPT')
+          res.status(400).json({ errorMessage: "User already booked" });
         const result = await
           db.booking.create({
-            work_at: req.body.work_at,
+            customer_location: req.body.customer_location,
+            work_date: req.body.work_date,
             work_hour: req.body.work_hour,
             status: 'WAIT_FOR_ACCEPTANCE',
             pay_slip_image: req.body.pay_slip_image,
-            employer_id: req.body.employer_id,
-            maid_id: req.body.maid_id
+            employer_id: req.user.id,
+            maid_id: maidId
           });
         if (result.length === 0) {
-          res.status(204).json({ result })
+          res.status(204).json({ result });
         } else {
-          res.status(200).json({ result })
+          res.status(200).json({ result });
         }
       } catch (err) {
-        if (err.message.includes('ECONNREFUSED')) {
-          res.status(500).json({ errorMessage: 'Database server error' })
+        if (err.message.includes("ECONNREFUSED")) {
+          res.status(500).json({ errorMessage: "Database server error" });
         }
-        res.status(400).json({ errorMessage: err.message })
+        res.status(400).json({ errorMessage: err.message });
       }
     },
 
     findBookingsByEmployerId: async (req, res) => {
       try {
-        const result = await
-          db.booking.findAll({
-            where: {
-              employer_id: req.params.employerId
-            }
-          });
+        const result = await db.booking.findAll({
+          where: {
+            employer_id: req.params.employerId
+          }
+        });
         if (result.length === 0) {
-          res.status(204).json(result)
+          res.status(204).json(result);
         } else {
-          res.status(200).json(result)
+          res.status(200).json(result);
         }
       } catch (err) {
-        if (err.message.includes('ECONNREFUSED')) {
-          res.status(500).json({ errorMessage: 'Database server error' })
+        if (err.message.includes("ECONNREFUSED")) {
+          res.status(500).json({ errorMessage: "Database server error" });
         }
-        res.status(400).json({ errorMessage: err.message })
+        res.status(400).json({ errorMessage: err.message });
       }
     },
 
     findBookingsByMaidId: async (req, res) => {
       try {
-        const maid = req.user
-        if(maid.type !== 'MAID') res.status(401).json({errorMessage: 'Unauthorized'})
+        const maid = req.user;
+        if (maid.type !== "MAID")
+          res.status(401).json({ errorMessage: "Unauthorized" });
         const result = await db.booking.findAll({
           where: {
             maidId: maid.id
           }
-        })
+        });
       } catch (err) {
-        if (err.message.includes('ECONNREFUSED')) {
-          res.status(500).json({ errorMessage: 'Database server error' })
+        if (err.message.includes("ECONNREFUSED")) {
+          res.status(500).json({ errorMessage: "Database server error" });
         }
-        res.status(400).json({ errorMessage: err.message })
+        res.status(400).json({ errorMessage: err.message });
       }
     }
-  }
+  };
 };
--- backend-maid-pro/routes/booking.router.js
+++ backend-maid-pro/routes/booking.router.js
@@ -2,7 +2,7 @@ const passport = require('passport')
 const bookingController = require('../controllers/booking.controller')
 module.exports = (server, db) => {
     const controller = bookingController(db)
-    server.post('/bookings', controller.createBooking);
+    server.post('/bookings/maids/:maidId', passport.authenticate('jwt', { session: false }), controller.createBooking);
     // ? should pass with auth
     server.get('/bookings/employers/:employerId', controller.findBookingsByEmployerId);
 
