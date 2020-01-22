ab131c78f196dcbd21849800c35915c7d2ffea48
Hon <dennis.mejen@gmail.com>
2020-01-22 17:25:22 +0700

    Fix service buttons in HomePage

--- frontend-maid-pro/src/pages/HomePage/HomePage.js
+++ frontend-maid-pro/src/pages/HomePage/HomePage.js
@@ -3,7 +3,7 @@ import "./HomePage.css";
 import Navbar from "../../components/Navbar/Navbar";
 import MaidCard from "../../components/MaidCard/MaidCard";
 import Footer from "../../components/Footer/Footer";
-import { Carousel, Row, Col } from "antd";
+import { Carousel, Row, Col, Button } from "antd";
 import { FaBuilding, FaHome } from "react-icons/fa";
 import axios from "../../config/api.service";
 import { dispatch } from "rxjs/internal/observable/range";
@@ -41,75 +41,78 @@ export class HomePage extends Component {
   render() {
     const { imageUrls, topMaids } = this.state;
     return (
-      <>
+      <div>
         <Navbar />
-        <div className="HomePage-container">
-          <Row
-            type="flex"
-            justify="center"
-            className="HomePage-Carousel-container"
-          >
-            <Col span={24}>
-              <Carousel autoplay>
-                {imageUrls.map((url, i) => (
-                  <div key={i + " Carousel"}>
-                    <img
-                      src={url}
-                      alt="Carousel"
-                      className="HomePage-Carousel-img"
-                    />
-                  </div>
-                ))}
-              </Carousel>
-            </Col>
-          </Row>
-        </div>
-        <div className="container HomePage-footer-margin">
-          <Row>
-            <Col style={{ marginTop: 20, textAlign: "center" }}>
-              <h2> Services </h2>
-            </Col>
-            <div className="HomePage-margin">
-              <Col span={12}>
-                <Row type="flex" justify="center" align="middle">
-                  <Col className="HomePage-text-center HomePage-q-card">
-                    <FaBuilding className="HomaPage-icon" />
-                    <p> Condo </p>
-                  </Col>
-                </Row>
-              </Col>
-              <Col span={12}>
-                <Row type="flex" justify="center" align="middle">
-                  <Col className="HomePage-text-center HomePage-q-card">
-                    <FaHome className="HomaPage-icon" />
-                    <p> Home </p>
-                  </Col>
-                </Row>
-              </Col>
-            </div>
-          </Row>
-        </div>
-        <div>
-          <Row type="flex" justify="center" style={{ marginTop: "20px" }}>
-            <Col>
-              <h2> Maid Recommended For You </h2>
-            </Col>
-          </Row>
-          <div className="HomePage-margin">
-            {topMaids.map(maid => (
-              <Col key={maid.id} span={12}>
-                <Row type="flex" justify="center" align="middle" style={{ marginBottom: "20px" }}>
-                  <Col>
-                    <MaidCard maid={maid}/>
-                  </Col>
-                </Row>
-              </Col>
-            ))}
-          </div>
-        </div>
+        <Row>
+          <Col>
 
+
+
+            <Row type="flex" justify="center" className="HomePage-Carousel-container">
+              <Col span={24}>
+                <Carousel autoplay>
+                  {imageUrls.map((url, i) => (
+                    <div key={i + " Carousel"}>
+                      <img src={url} alt="Carousel" className="HomePage-Carousel-img" />
+                    </div>
+                  ))}
+                </Carousel>
+              </Col>
+            </Row>
+
+
+
+            <Row type="flex" justify="center">
+              <h2>Services</h2>
+            </Row>
+            <Row>
+              <Col span={12}>
+                <Row type="flex" justify="end">
+                  <Button className="HomePage-ServiceButtons">
+                    <Col className="HomePage-ButtonIconColumn">
+                      <FaBuilding className="HomePage-ButtonColumnIcon" />
+                    </Col>
+                    <Col>Condo</Col>
+                  </Button>
+                </Row>
+              </Col>
+              <Col span={12}>
+                <Row type="flex" justify="start">
+                  <Button className="HomePage-ServiceButtons">
+                    <Col className="HomePage-ButtonIconColumn">
+                      <FaHome className="HomePage-ButtonColumnIcon" />
+                    </Col>
+                    <Col>Home</Col>
+                  </Button>
+                </Row>
+              </Col>
+            </Row>
+
+
+
+            <Row type="flex" justify="center" style={{ marginTop: "20px" }}>
+              <Col>
+                <h2> Maid Recommended For You </h2>
+              </Col>
+            </Row>
+            <Row>
+              {topMaids.map(maid => (
+                <Col key={maid.id} span={12}>
+                  <Row type="flex" justify="center" align="middle" style={{ marginBottom: "20px" }}>
+                    <Col>
+                      <MaidCard maid={maid} />
+                    </Col>
+                  </Row>
+                </Col>
+              ))}
+            </Row>
+
+
+
+          </Col>
+        </Row>
         <Footer />
-      </>
+      </div>
     );
   }
 }
@@ -121,4 +124,4 @@ const mapDispatchToProps = dispatch => {
   };
 };
 
-export default connect( null, mapDispatchToProps )(HomePage)
+export default connect(null, mapDispatchToProps)(HomePage)
