8f5682601f93614fbe2cb5d72aa379547fef58c3
fResult <styxmaz@gmail.com>
2020-01-24 16:32:17 +0700

    commit review seeder

--- backend-maid-pro/seeders/5.review-seed.js
+++ backend-maid-pro/seeders/5.review-seed.js
@@ -1,12 +1,12 @@
-const faker = require('faker');
-const { numOfUser } = require('../utils/index');
+const faker = require("faker");
+const { numberOfUser } = require("../utils/index");
 
 const reviewData = [];
-for (let i = 2, id = 1; i <= numOfUser; i += 2, id++) {
-  for (let j = 2; j <= numOfUser; i += 2) {
+for (let i = 1, id = 1; i <= numberOfUser; i += 2, id++) {
+  for (let j = 2; j <= numberOfUser; j += 2) {
 
     let data = {
-      // id,
+      id,
       content: faker.lorem.line(),
       rating: Math.floor(Math.random() * 5) + 1,
       employer_id: i,
@@ -15,7 +15,7 @@ for (let i = 2, id = 1; i <= numOfUser; i += 2, id++) {
       updatedAt: new Date()
     };
 
-    console.log('data', data);
+    console.log(data);
     reviewData.push(data)
   }
 }
