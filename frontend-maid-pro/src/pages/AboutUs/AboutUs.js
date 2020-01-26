058119b101078ea773c1cae431d5a34d2e6f9cf2
Hon <dennis.mejen@gmail.com>
2020-01-26 17:45:20 +0700

    Save progress on AboutUs page

---
+++ frontend-maid-pro/src/pages/AboutUs/AboutUs.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import './AboutUs.css'
+
+export default class AboutUs extends Component {
+  render() {
+    return (
+      <div>
+        Hello
+      </div>
+    )
+  }
+}
